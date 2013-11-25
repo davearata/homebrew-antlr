35cfe228637e720b182ee5bb5fd1f4a29fb0a591
dave <dave.arata@dacatech.com>
2013-11-25 08:43:40 -0500

    Adding antlr 3.1.3

---
+++ Formula/antlr-3_1_3.rb
@@ -0,0 +1,12 @@
+require 'formula'
+
+class Antlr < Formula
+  homepage 'http://www.antlr.org/'
+  url 'http://www.antlr3.org/download/antlr-3.1.3.jar'
+  sha1 'acd5253cf1eba7bdb133f14cd77b0ba2fd219f98'
+
+  def install
+    prefix.install "antlr-3.1.3.jar"
+    bin.write_jar_script prefix/"antlr-3.1.3.jar", "antlr-3.1.3"
+  end
+end
