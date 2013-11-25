13e6006d6e5a1ca236c55836b74b0f69d6629463
dave <dave.arata@dacatech.com>
2013-11-25 08:49:09 -0500

    changed class name

--- Formula/antlr-3_1_3.rb
+++ Formula/antlr-3_1_3.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class Antlr < Formula
+class Antlr313 < Formula
   homepage 'http://www.antlr.org/'
   url 'http://www.antlr3.org/download/antlr-3.1.3.jar'
   sha1 'acd5253cf1eba7bdb133f14cd77b0ba2fd219f98'
